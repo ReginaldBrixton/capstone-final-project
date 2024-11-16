8b5c92bdd0b9799876944a444d0a06d93291e51a
Reginald Emmanuel Brixton <reginaldbrixton@gmail.com>
2024-11-16 18:59:18 +0000

    "Refactor auth components: update login and register forms, add phone and Google sign-in options, and modify Firebase authentication functions."

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'capstone-final-project',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { getDataConnect, validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'capstone-final-project',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
