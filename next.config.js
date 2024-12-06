4c3fadfa36add17b26138f200b85957514e7dde4
Reginald Emmanuel Brixton <reginaldbrixton@gmail.com>
2024-12-06 04:20:48 +0000

    "Update Jest config and setup, add new aliases and ignore patterns, and modify test files"

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: ['./src/styles'],
+  images: {
+    domains: ['images.unsplash.com'],
   },
-};
+}
 
-module.exports = nextConfig; 
+module.exports = nextConfig 
